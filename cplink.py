3601734b1322d3eb574de20d3712e7b6cb54ee37
Your Name <rosser.bjr@gmail.com>
2014-08-04 01:04:26 -0400

    wrote argument parser, began writing script.

--- cplink.py
+++ cplink.py
@@ -18,9 +18,30 @@ import argparse
 import os
 import sys
 
+def cplink(directory, verbose=False):
+	try:
+		source = readlink(directory)
+		os.remove(directory)
+		shutil.copytree(source, directory)
+	except:
+		# Because isn't this everyone's favorite error message?
+		print "Error: no such file or directory."
+
 def main():
 	"""Main function of script."""
-	
+	parser = argparse.ArgumentParser(description="Tool to unlink two directories and make a unique copy in place of the link.")
+	parser.add_argument("directory", metavar="DIR", type=str, help="The directory to unlink and replace.")
+	parser.add_argument("-r", "--recursive", action="store_true", dest="recursive", help="Recursively fix all links in DIR.")
+	parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="Enable verbose command line output.")
+	args = parser.parse_args()
+
+	# Do some sanitation
+	# I know that "it's better to ask forgiveness than permission."
+	directory = os.path.abspath(args.directory)
+	if not os.path.exists(directory):
+		print "Error: no such directory."
+
+
 
 if __name__ == '__main__':
 	main()
